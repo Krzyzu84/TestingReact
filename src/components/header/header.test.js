1e9314f54842d781d19399701c0bbd3a2d44f132
unknown <krzysztofptak97@gmail.com>
2020-09-08 15:16:17 +0200

    Header Component and Tests

---
+++ src/components/header/header.test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Header from "./index";
+import { findByTestAtrr } from "../../../../Utils/index";
+
+const setUp = (props = {}) => {
+  const component = shallow(<Header {...props} />);
+  return component;
+};
+
+describe("Header Component", () => {
+  let component;
+  beforeEach(() => {
+    component = setUp();
+  });
+
+  it("should render without errors", () => {
+    const wraper = findByTestAtrr(component, "headerComponent");
+    expect(wraper.length).toBe(1);
+  });
+
+  it("Should render a logo", () => {
+    const logo = findByTestAtrr(component, "logoIMG");
+    expect(logo.length).toBe(1);
+  });
+});
